f9678cf6c8413cf5a106fbd987def857c00627f6
rykunk21 <rykunk21@gmail.com>
2021-01-25 18:34:39 -0500

    25Jan lets do some web dev

---
+++ CSE-232/Week02/hw2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+
+int main(){
+  string first, last;
+  long my_int;
+  cin >> first >> my_int >> last;
+  cout << last << endl << first << endl << my_int << endl ;
+}
