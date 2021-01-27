fb6ccce8a6e37fb4dde677e2aa0ebc2052ae287d
rykunk21 <rykunk21@gmail.com>
2021-01-27 14:51:01 -0500

    update file organization

---
+++ CSE-232/lab01/lab01.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using std::cout; using std::cin; using std::endl; using std::fixed;
+#include<iomanip>
+using std::setprecision;
+
+int main() {
+    cout << fixed << setprecision(2);
+    // Your code here
+    return 0;
+}
